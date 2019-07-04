3571aa9f83082705a0575ba3e7dfbb310fd5541a
manimv <manimv@DIN78000415.corp.capgemini.com>
2019-07-04 13:12:59 +0530

    My first git commit

---
+++ 1stproject/src/main/java/MainClass.java
@@ -0,0 +1,14 @@
+
+public class MainClass {
+	public static void main(String[] args)
+	{
+		System.out.println("welcome Sales Application");
+		salesclass obj=new salesclass();
+	obj.show();	
+	}
+	public static void display() {
+		System.out.println("Be happy!");
+		
+	}
+	
+}
---
+++ 1stproject/src/main/java/salesclass.java
@@ -0,0 +1,10 @@
+
+public class salesclass {
+	private int[] arr= {2,3,4,5};
+	public void show() {
+		for(int num:arr) {
+			System.out.println(num);
+		}
+	}
+
+}
